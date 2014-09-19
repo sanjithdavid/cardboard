070e2226ba611749aa11ccf959acbc95f807dfb4
sanjithdavid <sdavid@deloitte.com>
2014-09-19 15:47:16 -0400

    first

---
+++ app/src/main/java/android/openglsdavid/com/my3d/OpenGLES20Activity.java
@@ -0,0 +1,43 @@
+package android.openglsdavid.com.my3d;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+/**
+ * Created by sanjithdavid on 9/19/14.
+ */
+public class OpenGLES20Activity extends Activity{
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // The following call pauses the rendering thread.
+        // If your OpenGL application is memory intensive,
+        // you should consider de-allocating objects that
+        // consume significant memory here.
+        mGLView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The following call resumes a paused rendering thread.
+        // If you de-allocated graphic objects for onPause()
+        // this is a good place to re-allocate them.
+        mGLView.onResume();
+    }
+
+}
